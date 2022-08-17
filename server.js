b7e87cfccc0cba36692fa4fe138799cb55d51762
ooowwwlll <75400842+owl-birds@users.noreply.github.com>
2022-08-17 14:28:25 +0700

    finished simple server with nodejs

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 // source : https://medium.com/swlh/how-to-build-a-javascript-web-server-from-scratch-using-only-node-js-a4e21a9abc88
-const http = require("http");
-
+var http = require("http");
 // vars
-const PORT = 3000;
-
+var PORT = 3000;
 // 
 // request and response parameter, handle the "request" to and the "response" from, our server
 // HTTP header is essentially a set of key/value pairs that provides information about a "request" or a "response", depending if it's on the "client" side or the "server" side.
-const server = http.createServer((request, response)=>{
-	response.setHeader("Content-Type", "text/plain");
-	response.end("HELLO WORLD!");
+var server = http.createServer(function (request, response) {
+    response.setHeader("Content-Type", "text/plain");
+    response.end("HELLO WORLD!");
 });
-
-
 // server will listen to port that we gave
-server.listen(PORT, ()=>{
-	console.log(`listening to ${PORT}`);
+server.listen(PORT, function () {
+    console.log("listening to ".concat(PORT));
 });
